07ee374e2161c3a194d26e177444a6b0120be530
Nihat <abdullazadenihat@gmail.com>
2023-09-06 01:52:59 +0400

    added login design

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -9,42 +9,45 @@ import './index.scss'
 
 export function AuthPage() {
   return (
-    <main className='auth-page'>
-      <div  style={{width : '600px'}} className='grid' >
-        <div className="left">
+    <main className='auth-page min-h-screen  bg-blue-800  flex justify-center items-center ' >
+      <div style={{ width: '800px' }} className='flex  shadow-md     ' >
+        <div className="w-1/2 bg-indigo-500 text-white px-6 py-4  "  >
           <div className="brand">
-            <div className="name"> TrendPixel </div>
-            <img src="" alt="logo" />
+            <h2 className="name text-xl font-normal"> TrendPixel </h2>
+            {/* <img src="" alt="logo" /> */}
           </div>
-          <h2 className="title"> Hey There ! </h2>
-          <p className="desc"> Welcome Back </p>
-          <p className="desc-2"> You are just one step away to your feed </p>
+          <div className='group flex flex-col items-center '>
+            <h2 className="title text-3xl font-bold text-center mt-10 mb-10" > Hey There ! </h2>
+            <p className="desc "> Welcome Back </p>
+            <p className="desc-2 "> You are just one step away to your feed </p>
 
+            <button className='mt-10  text-slate-200'> Don't have an account? </button>
+            <button className="mt-4 bg-transparent hover:bg-purple-500 text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded"> Sign Up </button>
+            <img className='dna-img mt-10' src={dnaSVG} alt="" />
 
-          <button> Don't have an account </button>
+          </div>
 
-          <img className='dna-img' src={dnaSVG} alt="" />
 
         </div>
-        <div className="right">
-          <h3 className="title"> SIGN IN </h3>
+        <div className="right w-1/2 bg-white px-6 py-4">
+          <h3 className="title font-bold text-2xl text-blue-500 mb-10"> SIGN IN </h3>
           <form action="">
             <AuthInput placeholder="Email" />
             <AuthInput placeholder="Password" />
           </form>
-          <div className="extra">
-            <div className="keep-me">
+          <div className="extra flex justify-between mt-7">
+            <div className="keep-me flex gap-2">
               <input type="checkbox" name="" id="" />
-              <p className="text"> Keep me logged in </p>
+              <p className="text text-gray-700 text-sm"> Keep me logged in </p>
             </div>
-            <button> Forgot Password ? </button>
+            <button className='text-orange-500 text-sm font-bold'> Forgot Password ? </button>
           </div>
 
-          <p className="social-media-text"> Or use social media to sign in  </p>
+          <p className="social-media-text mt-10"> Or use social media to sign in  </p>
 
-          <div className="social-media-icons">
-            <img src={facebookSvg} alt="" />
-            <img src={twitterSvg} alt="" />
+          <div className="social-media-icons mt-10 flex justify-center gap-8">
+            <img src={facebookSvg} alt="" width={"24px"} />
+            <img src={twitterSvg} alt="" width={"24px"} />
 
           </div>
 
